01278567e467179079f396768aa33e9cfb33b651
Asset Sultanov <asset.sultan@gmail.com>
2018-06-15 18:56:31 +0600

    some commit for checking

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res, next)=>{
 	res.send('Home Page')
 })
 
+app.get('/about', (req, res, next)=>{
+	res.send('About Page')
+})
+
 app.listen(3000, ()=>{
 	console.log('Server started on port 3000...')
 })
